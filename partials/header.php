468a0332a82339f16e4d8078bdb2a0f0f5c54885
panos1997 <33810048+panos1997@users.noreply.github.com>
2019-01-06 19:49:54 +0200

    Update header.php

--- partials/header.php
+++ partials/header.php
@@ -18,7 +18,6 @@
     <!-- Custom styles for this template -->
     <link href="../css/app.css" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
-	<link rel="stylesheet" href="../css/style.css">
 
   </head>
 

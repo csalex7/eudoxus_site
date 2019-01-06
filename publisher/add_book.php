8d0500c990d4f75aa0db234587540dbb511e2d63
csalex7 <33809617+csalex7@users.noreply.github.com>
2019-01-06 19:44:58 +0200

    Add files via upload

---
+++ publisher/add_book.php
@@ -0,0 +1,16 @@
+<?php
+	include_once '../Config.php';
+
+	$title= $_POST['title'];
+	$isbn= $_POST['ISBN'];
+	$author= $_POST['author'];
+	$category= $_POST['category'];
+	$available_books= $_POST['available_books'];
+	$publisher_username=$_GET['username'];
+
+	$query= "INSERT INTO suggrammata(title,isbn,author,category,quantity,publisher_username) VALUES('$title','$isbn','$author','$category','$available_books','$publisher_username');";
+	mysqli_query($db,$query);
+
+	header("refresh:1; url=publisher2.php");
+
+?>

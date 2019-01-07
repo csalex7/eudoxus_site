bb08d6c0e1a7669e2964f130db9ee56903327ede
csalex7 <33809617+csalex7@users.noreply.github.com>
2019-01-07 18:05:26 +0200

    Update publisher2.php

--- publisher/publisher2.php
+++ publisher/publisher2.php
@@ -17,31 +17,40 @@ include('../partials/header.php');
 
 
 <div class="book_list">
-  <?php
   
+
+  <?php
   $query="SELECT * FROM suggrammata WHERE publisher_username='publisher2';";
   $result = mysqli_query($db,$query);
   $resultCheck= mysqli_num_rows($result);
   if ($resultCheck > 0) {
     while($row= mysqli_fetch_assoc($result)){
+    ?>
+   	  <p class="book_container">
+   	 <?php
+
       echo "Τίτλος:". $row['title']; echo "<br>"; 
       echo "ISBN:". $row['isbn']; echo "<br>";
       echo "Συγγραφέας:". $row['author']; echo "<br>";
       echo "Κατηγορία:". $row['category']; echo "<br>";
       echo "Διαθέσιμα Αντίτυπα:". $row['quantity']; echo "<br>";
-      echo "<a href=delete_book.php?id=".$row['id'].">Αφαίρεση</a>";
+      echo "<a href=delete_book.php?id=".$row['id'].">Αφαίρεση</a>"; echo "<br>";
       echo "<a href=update_book.php?id=".$row['id'].">Αλλαγή στοιχείων</a>";
+      ?>
+      </p>
+        <?php
       echo "<br>";
-      echo "<br>";
-      
+       ?>
+ <?php
     }
   } 
  
   ?>
+
 </div>
 
 
-<a href="publisher.php" class="previous">&#8678;Πίσω</a>
+<a href="publisher.php" class="previous2">&#8678;Πίσω</a>
 <a href="publisher3.php" class="publisher_add">Προσθήκη συγγράμματος</a>
 
 

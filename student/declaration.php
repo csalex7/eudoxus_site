0af6182988c6ae8ed90e826e6d10e043b334bcc7
panos1997 <33810048+panos1997@users.noreply.github.com>
2019-01-08 12:45:26 +0200

    Create declaration.php

---
+++ student/declaration.php
@@ -0,0 +1,23 @@
+<?php
+include_once('../partials/header.php');
+?>
+
+
+<nav aria-label="breadcrumb">
+  <ol class="breadcrumb container">
+    <li class="breadcrumb-item"><a href="/">Αρχική</a></li>
+    <li class="breadcrumb-item active" aria-current="page">Φοιτητής</li>
+    <li class="breadcrumb-item active" aria-current="page">Δήλωση Μαθημάτων</li>
+        <li class="options">
+          <p id="option1"><a class="badge badge-light" href="/signin">ΒΟΗΘΕΙΑ</a></p>
+          <p id="option12"><a class="badge badge-light" href="/signin">ΑΝΑΖΗΤΗΣΗ ΒΙΒΛΙΩΝ</a></p>
+        </li>
+  </ol>
+</nav>
+
+
+
+
+<?php
+include_once('../partials/footer.html');
+?>

b603176049866b4380b13cc6e2fec5d22e06630e
panos1997 <33810048+panos1997@users.noreply.github.com>
2019-01-10 22:39:29 +0200

    Add files via upload

--- partials/header.php
+++ partials/header.php
@@ -1,6 +1,6 @@
 <?php
     if(!isset($_SESSION))
-    { 
+    {
         session_start();
     }
 ?>
@@ -20,13 +20,14 @@
 
     <title>ΕΥΔΟΞΟΣ</title>
 
-    <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <!-- Custom styles for this template -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+
     <link href="../css/app.css" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 
-    <script type="text/javascript" src="http://code.jquery.com/jquery-1.9.1.js"></script>
     <script src="../student/student.js"></script>
   </head>
 

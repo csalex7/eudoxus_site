3abfb6f30183314a90e84bb3d0f9c824ae02290c
csalex7 <33809617+csalex7@users.noreply.github.com>
2019-01-10 15:08:12 +0200

    Update header.php
    line 98 allagi path gia logout

--- partials/header.php
+++ partials/header.php
@@ -95,7 +95,7 @@
           </div>
         </li>
              <?php if(isset($_SESSION['login_user'])){  ?>
-                    <li class="signUpIn" ><a  class="badge badge-light" href="../eudoxus_site/logout.php">ΑΠΟΣΥΝΔΕΣΗ<i class="fas fa-user-plus"></i></a></li>
+                    <li class="signUpIn" ><a  class="badge badge-light" href="/eudoxus_site/logout.php">ΑΠΟΣΥΝΔΕΣΗ<i class="fas fa-user-plus"></i></a></li>
             <?php }
              else {   ?>
                     <li class="signUpIn"><a class="badge badge-light" href="/eudoxus_site/login.php">ΕΙΣΟΔΟΣ<i class="fas fa-user"></i></a></li>

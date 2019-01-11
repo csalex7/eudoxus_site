38b3a03b8a5c1e1ad72b8c6dbe8e8b886dfdccfe
csalex7 <33809617+csalex7@users.noreply.github.com>
2019-01-11 18:27:05 +0200

    Update book_list.php

--- student/book_list.php
+++ student/book_list.php
@@ -1,5 +1,10 @@
 <?php
 include_once '../Config.php';
+session_start();
+if(!isset($_SESSION['login_user'])){
+         echo "not set ";
+         header("location: ../login.php");
+    }
 include_once '../partials/header.php';
 include_once '../partials/footer.html';
 ?>

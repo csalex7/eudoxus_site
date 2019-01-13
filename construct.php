4765e1556f05d1a6cd17cc4513d9cb118704f139
Nikos <nikotheract11@gmail.com>
2019-01-13 18:50:55 +0200

    add cosntruct page

---
+++ construct.php
@@ -0,0 +1,11 @@
+<?php
+   include('partials/header.php');
+?>
+<link href="css/app.css" rel="stylesheet">
+<div class="container">
+<h1> Σελίδα υπό κατασκευή </h1>
+</div>
+<?php
+   include('partials/footer.html');
+?>
+

6a6f2ce008fcf34b53ed8ca9da496241c33a58fa
panos1997 <33810048+panos1997@users.noreply.github.com>
2019-01-13 19:39:37 +0200

    Update Config.php

--- Config.php
+++ Config.php
@@ -1,5 +1,5 @@
 <?php
-   define('DB_SERVER', 'localhost:3336');
+   define('DB_SERVER', 'localhost:3306');
    define('DB_USERNAME', 'root');
    define('DB_PASSWORD', '');
    define('DB_DATABASE', 'testdb');
